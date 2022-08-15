93fb079c638f706c00c4503ebb7840be2a3a5b76
Carl Duncan <carl.duncan@gkco.com>
2022-08-15 18:23:31 -0500

    feat: changed the webApp url to the production url

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,6 +1,6 @@
 class Constant {
   static const int load = 1000;
-  static const String webApp = "https://help-us-web-4932.web.app/";
+  static const String webApp = "https://help-us-app.com/";
   static const String twitter = "https://twitter.com/";
   static const String instagram = "https://instagram.com/";
 }
